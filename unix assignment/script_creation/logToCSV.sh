9ec0503159952e510ce185498835b64564855a36
Ágoston Szabó <Szabo-2@student.tudelft.nl>
2024-10-02 19:40:25 +0200

    logToCSV is implemented, but needs debugging

--- unix assignment/script_creation/logToCSV.sh
+++ unix assignment/script_creation/logToCSV.sh
@@ -8,3 +8,23 @@
 # This can be done by piping the output to a file.
 # Example: `./logToCSV access_log > output.csv`
 # It could take some time to convert all of the `access_log` file contents. Consider using a small subset for testing.
+
+fileIn=$1
+
+client=$(cat $fileIn | cut -d ' ' -f 1)
+
+time=$(cat $fileIn | cut -d ' ' -f 4 | tr -d '[')
+
+type=$(cat $fileIn | cut -d '"' -f 2 | cut -d ' ' -f 1)
+
+#path=$(cat $fileIn | tr '"-"' '"- - -"' | cut )
+#echo "$type"
+
+
+#helper=$(cat $1 | grep ".*telia.*")
+#echo "$helper"
+result=$(cat $fileIn | sed -e 's/"-"/"- - -"/' | cut -d ' ' -f 1,4,6,7,9,10 | tr -d '[' | tr -d '"' | tr ' ' ',')
+
+echo "$result" > output.csv
+# echo $(cat "$1" | wc -l)
+# echo "$result" | wc -l
