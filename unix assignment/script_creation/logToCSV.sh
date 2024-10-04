d3a02e0afd8c39b8bb77241d22bc06aa1b765d51
Arnon Zandt <a.q.zandt@student.tudelft.nl>
2024-10-04 15:59:27 +0200

    write implementation for logToCSV

--- unix assignment/script_creation/logToCSV.sh
+++ unix assignment/script_creation/logToCSV.sh
@@ -8,3 +8,15 @@
 # This can be done by piping the output to a file.
 # Example: `./logToCSV access_log > output.csv`
 # It could take some time to convert all of the `access_log` file contents. Consider using a small subset for testing.
+
+# line="h24-71-236-129.ca.shawcable.net - - [11/Mar/2004:12:28:51 -0800] \"GET /icons/gnu-head-tiny.jpg HTTP/1.1\" 304 -"
+# echo $line | sed -E 's/ - - /,/;s/\[//;s/\]//;s/ "/,/;s/ \//,/;s/ ([A-Z]|[a-z])+\/[0-9].[0-9]" /,/;s/ ([0-9])/,&/;s/, /,/;s/[0-9][0-9][0-9] -/&???/;s/ -\?\?\?/,-/'
+sed -E 's/ - - /,/;s/\[//;s/\]//;s/ "/,/;s/ \//,\//;s/ ([A-Z]|[a-z])+\/[0-9].[0-9]" /,/;s/ ([0-9])/,&/;s/, /,/;s/[0-9][0-9][0-9] -/&???/;s/ -\?\?\?/,-/' $1
+
+# sed -E 's/ - - /,/        separate Client and Time
+#         ;s/\[//;s/\]//    Remove [] from Time
+#         ;s/ "/,/          separate Time and Type
+#         ;s/ \//,\//       separate Type and Path
+#         ;s/ ([A-Z]|[a-z])+\/[0-9].[0-9]" /,/      remove Version
+#         ;s/ ([0-9])/,&/;s/, /,/   separate Status and Path
+#         ;s/[0-9][0-9][0-9] -/&???/;s/ -\?\?\?/,-/'    separate Status and Size
